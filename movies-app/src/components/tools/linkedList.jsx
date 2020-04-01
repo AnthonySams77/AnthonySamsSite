d2bf9c6b88797128f2ec0c1930e551424c3d23d2
AnthonySams77 <42755588+AnthonySams77@users.noreply.github.com>
2020-04-01 16:46:55 -0500

    created linked list class

---
+++ movies-app/src/components/tools/linkedList.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import Node from "./node";
+
+class LinkedList extends Component {
+  state = {
+    first,
+    last
+  };
+
+  addFirst(value) {
+    if (this.first === null) {
+      this.first = this.last = node;
+    }
+
+    node = Node(value, first);
+    this.first = node;
+  }
+
+  addLast(value) {
+    node = Node(value, null);
+
+    if (first === null) this.first = this.last = node;
+    else {
+      this.last.next = node;
+      this.last = node;
+    }
+  }
+
+  deleteFirst() {
+    first = this.first.next;
+  }
+
+  deleteLast() {
+    current = this.first;
+    last;
+
+    while (current.next !== null) {
+      current = current.next;
+      last = current;
+    }
+    this.last = last;
+  }
+
+  contains(value) {
+    current = this.first;
+    while (value !== current.value) {
+      current = current.next;
+    }
+    if (current.value === value) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  indexOf(value) {
+    current = this.first;
+    count = 0;
+    while (value !== current.value) {
+      current = current.next;
+      count++;
+    }
+    if (current.value === value) {
+      return count;
+    } else {
+      return -1;
+    }
+  }
+
+  isEmpty() {}
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default LinkedList;
---
+++ movies-app/src/components/tools/node.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Node extends Component {
+  state = {
+    value,
+    next
+  };
+
+  Node(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Node;
